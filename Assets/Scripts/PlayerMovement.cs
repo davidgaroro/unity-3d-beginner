c4bfb5d3eb68debf307f25a94739797f2c7b239d
davidgaroro <davidgaroro@gmail.com>
2019-06-15 22:01:34 +0200

    2.12 Adjust the turnSpeed variable

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float turnSpeed;
+    public float turnSpeed = 20f;
     Animator m_Animator;
     Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
     
     // Start is called before the first frame update
     void Start()
@@ -29,5 +31,6 @@ public class PlayerMovement : MonoBehaviour
       m_Animator.SetBool("IsWalking", isWalking);
 
       Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+      m_Rotation = Quaternion.LookRotation(desiredForward);
     }
 }
