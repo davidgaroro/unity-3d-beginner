b2a6e4e326dd66716973b1caa6c0826d465a5122
davidgaroro <davidgaroro@gmail.com>
2019-06-15 21:10:39 +0200

    2.3 Create variables for the Horizontal and Vertical axes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      float horizontal = Input.GetAxis("Horizontal");
     }
 }
