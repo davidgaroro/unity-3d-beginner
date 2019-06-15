2cd29598c8a211bf6e390192b81067aafc3cb9d4
davidgaroro <davidgaroro@gmail.com>
2019-06-15 22:08:25 +0200

    2.14 Movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -38,6 +38,6 @@ public class PlayerMovement : MonoBehaviour
 
     void OnAnimatorMove()
     {
-      
+      m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
     }
 }
