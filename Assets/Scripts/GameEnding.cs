936495aa11504ccce000de044b576b194d6e98f9
davidgaroro <davidgaroro@gmail.com>
2019-06-20 00:55:41 +0200

    5.12 Write the if Statement Code Block

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -5,20 +5,37 @@ using UnityEngine;
 public class GameEnding : MonoBehaviour
 {
   public float fadeDuration = 1f;
+  public float displayImageDuration = 1f;
   public GameObject player;
+  public CanvasGroup exitBackgroundImageCanvasGroup;
+  
   bool m_IsPlayerAtExit;
+  float m_Timer;
 
-  void OnTriggerEnter(Collider other) {
-    if(other.gameObject == player)
+  void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject == player)
       {
         m_IsPlayerAtExit = true;
       }
   }
 
-  void Update() {
+  void Update()
+  {
     if(m_IsPlayerAtExit)
       {
-
+        EndLevel();
       }
+  }
+
+  void EndLevel()
+  {
+    m_Timer += Time.deltaTime;
+    exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+    if (m_Timer > fadeDuration + displayImageDuration)
+    {
+      Application.Quit();
+    }
   }
 }
